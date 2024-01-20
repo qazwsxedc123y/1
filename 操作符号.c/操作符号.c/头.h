2130c824b06084c478be070d16007b095e953a0b
simple-and-silly-matcha-c <1476303287@qq.com>
2024-01-20 22:48:20 +0800

    1.20

--- 操作符号.c/操作符号.c/头.h
+++
@@ -1,7 +0,0 @@
-#define  _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>//standard library  标准库  对应system
-#include<windows.h>//程序需要的重要头文件
-#include<math.h>
-#include<time.h>
