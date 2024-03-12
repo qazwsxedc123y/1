f4936763738dd289ad82a300f5c21650bcdda482
simple-and-silly-matcha-c <1476303287@qq.com>
2024-03-12 22:33:33 +0800

    ss

--- 练习/test.c
+++ 练习/test.c
@@ -4,15 +4,205 @@
 #include<string.h>
 
 
-int main()
-{
-	char arr1[] = "this is an apple";
-	char arr2[] = "is";
-	char* ret = strstr(arr1, arr2);
-	printf("%s\n", ret);
-	printf("%s\n", arr1);
-	return 0;
-}
+
+
+
+
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//struct ListNode* middleNode(struct ListNode* head) {
+//    struct ListNode* slow = head, * fast = head;
+//    while (fast && fast->next)
+//    {
+//        slow = slow->next;
+//        fast = fast->next->next;
+//    }
+//    return slow;
+//}
+
+
+
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//struct ListNode* reverseList(struct ListNode* head) {
+//    struct ListNode* cur = head, * tail = NULL, * p2 = NULL;
+//    while (cur)
+//    {
+//        tail = cur;
+//        cur = cur->next;
+//        tail->next = p2;
+//        p2 = tail;
+//    }
+//    return tail;
+//}
+
+
+
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//struct ListNode* removeElements(struct ListNode* head, int val) {
+//    struct ListNode* tail = head, * cur = NULL;
+//    while (tail)
+//    {
+//        if (tail->val == val)
+//        {
+//            if (cur)
+//            {
+//                cur->next = tail->next;
+//                free(tail);
+//                //µø´ú
+//                tail = cur->next;
+//            }
+//            else
+//            {
+//                tail = head->next;
+//                free(head);
+//                head = tail;
+//
+//            }
+//        }
+//        else
+//        {
+//            cur = tail;
+//            tail = cur->next;
+//        }
+//    }
+//    return head;
+//}
+
+
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+//    struct ListNode* head = NULL, * tail = NULL;
+//    if (list1 == NULL)
+//    {
+//        return list2;
+//    }
+//    if (list2 == NULL)
+//    {
+//        return list1;
+//    }
+//    while (list1 && list2)
+//    {
+//        if (list1->val <= list2->val)
+//        {
+//            if (tail == NULL)
+//            {
+//                head = tail = list1;
+//            }
+//            else
+//            {
+//                tail->next = list1;
+//                tail = tail->next;
+//            }
+//            list1 = list1->next;
+//        }
+//        else
+//        {
+//            if (tail == NULL)
+//            {
+//                head = tail = list2;
+//            }
+//            else
+//            {
+//                tail->next = list2;
+//                tail = tail->next;
+//            }
+//            list2 = list2->next;
+//        }
+//    }
+//    if (list1)//1½áÊø
+//    {
+//        tail->next = list1;
+//    }
+//    else
+//    {
+//        tail->next = list2;
+//    }
+//    return head;
+//}
+
+
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+//    struct ListNode* head = NULL, * tail = NULL;
+//    head = tail = (struct ListNode*)malloc(sizeof(struct ListNode));
+//    if (list1 == NULL)
+//    {
+//        return list2;
+//    }
+//    if (list2 == NULL)
+//    {
+//        return list1;
+//    }
+//    while (list1 && list2)
+//    {
+//        if (list1->val <= list2->val)
+//        {
+//            tail->next = list1;
+//            tail = list1;
+//
+//            list1 = list1->next;
+//        }
+//        else
+//        {
+//            tail->next = list2;
+//            tail = list2;
+//
+//            list2 = list2->next;
+//        }
+//    }
+//    if (list1)//1½áÊø
+//    {
+//        tail->next = list1;
+//    }
+//    else
+//    {
+//        tail->next = list2;
+//    }
+//    struct ListNode* del = head;
+//    head = head->next;
+//    free(del);
+//    return head;
+//}
+
+//int main()
+//{
+//	char arr1[] = "this is an apple";
+//	char arr2[] = "is";
+//	char* ret = strstr(arr1, arr2);
+//	printf("%s\n", ret);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
 
 
 //typedef struct SLList
