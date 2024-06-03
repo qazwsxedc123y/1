a82d6eb6ceacec2ce211cadb4772b93d0f40e184
simple-and-silly-matcha-c <1476303287@qq.com>
2024-06-03 21:16:16 +0800

    list

---
+++ list/test.cpp
@@ -0,0 +1,15 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+#include"list.h"
+int main()
+{
+	test::test_list1();
+	//test::test_list2();
+	//test::test_list3();
+
+	return 0;
+}
