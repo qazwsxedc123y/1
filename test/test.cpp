9c5a743adf2bb3a78efd51c6e91096a51a106350
simple-and-silly-matcha-c <1476303287@qq.com>
2024-06-22 21:43:09 +0800

    ss

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,6 @@
-//#define  _CRT_SECURE_NO_WARNINGS
+#define  _CRT_SECURE_NO_WARNINGS
 //
-//#include<iostream>
-//using namespace std;
+
 //
 //class Solution {
 //public:
@@ -32,3 +31,87 @@
 //    cout << val << endl;
 //    memmove()
 //}
+
+
+
+//#include<iostream>
+//#include<vector>
+//#include <algorithm>
+//using namespace std;
+//
+//class A
+//{
+//protected:
+//	int _a;
+//};
+//
+//class B : virtual public A
+//{
+//protected:
+//	int _b;
+//};
+//
+//class C : virtual public A
+//{
+//protected:
+//	int _c;
+//};
+//
+//class D :public B, public C
+////class D 
+//{
+//protected:
+//	int _d;
+//};
+//int main()
+//{
+//	cout << sizeof(D) << endl;
+//	return 0;
+//}
+
+
+#include<iostream>
+using namespace std;
+class A {
+public:
+	A(const char* s) { cout << s << endl; }
+	~A() {}
+};
+
+//class B :virtual public A
+class B : public A
+{
+public:
+	B(const char* s1, const char* s2) :A(s1) { cout << s2 << endl; }
+};
+
+class C : public A
+{
+public:
+	C(const char* s1, const char* s2) :A(s1) { cout << s2 << endl; }
+};
+
+class D : public C, public B
+{
+public:
+	//D(const char* s1, const char* s2, const char* s3, const char* s4) 
+	//	:B(s1, s2)
+	//	, C(s1, s3)
+	//	, A(s1)
+	//{
+	//	cout << s4 << endl;
+	//}
+
+	D(const char* s1, const char* s2, const char* s3, const char* s4)
+		:B(s1, s2)
+		, C(s1, s3)
+	{
+		cout << s4 << endl;
+	}
+};
+
+int main() {
+	D* p = new D("class A", "class B", "class C", "class D");
+	delete p;
+	return 0;
+}
