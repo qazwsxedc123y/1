f4a989ee89fd575ef5b9282f08e5509052eacde9
simple-and-silly-matcha-c <1476303287@qq.com>
2024-07-10 22:03:55 +0800

    710

--- write-2/test.cpp
+++ write-2/test.cpp
@@ -5,6 +5,168 @@
 #include<list>
 #include<stack>
 using namespace std;
+
+
+namespace bite
+{
+    // List的节点类
+    template<class T>
+    struct ListNode
+    {
+        ListNode(const T& val = T())
+            :_pPre(nullptr)
+            , _pNext(nullptr)
+            ,_val(val)
+        {}
+        ListNode<T>* _pPre;
+        ListNode<T>* _pNext;
+        T _val;
+    };
+
+
+    //List的迭代器类
+    template<class T, class Ref, class Ptr>
+    class ListIterator
+    {
+        typedef ListNode<T>* PNode;
+        typedef ListIterator<T, Ref, Ptr> Self;
+    public:
+        ListIterator(PNode pNode = nullptr)
+            :_pNode(pNode)
+        {}
+        ListIterator(const Self& l);
+        T& operator*()
+        {
+            return  &_pNode->_val;
+        }
+        T* operator->()
+        {
+            return _pNode->_val;
+        }
+        Self& operator++()
+        {
+            _pNode = _pNode->_pNext;
+            return *this;
+        }
+        Self operator++(int)
+        {
+            Self tmp(*this);
+            _pNode = _pNode->_pNext;
+            return tmp;
+        }
+        Self& operator--()
+        {
+            _pNode = _pNode->_pPre;
+            return *this;
+        }
+        Self& operator--(int)
+        {
+            Self tmp(*this);
+            _pNode = _pNode->_pPre;
+            return tmp;
+        }
+        bool operator!=(const Self& l)
+        {
+            return !(_pNode == Self._pNode);
+        }
+        bool operator==(const Self& l)
+        {
+            return _pNode == Self._pNode;
+        }
+    private:
+        PNode _pNode;
+    };
+
+
+    //list类
+    template<class T>
+    class list
+    {
+        typedef ListNode<T> Node;
+        typedef Node* PNode;
+    public:
+        typedef ListIterator<T, T&, T*> iterator;
+        typedef ListIterator<T, const T&, const T&> const_iterator;
+    public:
+        ///////////////////////////////////////////////////////////////
+        // List的构造
+        list()
+        {
+            CreateHead();
+        }
+        list(int n, const T& value = T())
+        {
+            int count = 0;
+            PNode cur = _pHead;
+            while (count--)
+            {
+                PNode tmp = new Node(value);
+                tmp->_pPre = cur;
+
+            }
+
+        }
+        template <class Iterator>
+        list(Iterator first, Iterator last);
+        list(const list<T>& l);
+        list<T>& operator=(const list<T> l);
+        ~list();
+
+
+        ///////////////////////////////////////////////////////////////
+        // List Iterator
+        iterator begin();
+        iterator end();
+        const_iterator begin();
+        const_iterator end();
+
+
+        ///////////////////////////////////////////////////////////////
+        // List Capacity
+        size_t size()const;
+        bool empty()const;
+
+
+        ////////////////////////////////////////////////////////////
+        // List Access
+        T& front();
+        const T& front()const;
+        T& back();
+        const T& back()const;
+
+
+        ////////////////////////////////////////////////////////////
+        // List Modify
+        void push_back(const T& val) { insert(end(), val); }
+        void pop_back() { erase(--end()); }
+        void push_front(const T& val) { insert(begin(), val); }
+        void pop_front() { erase(begin()); }
+        // 在pos位置前插入值为val的节点
+        iterator insert(iterator pos, const T& val)
+        {
+            PNode* cur = pos.PNode;
+        }
+        // 删除pos位置的节点，返回该节点的下一个位置
+        iterator erase(iterator pos);
+        void clear();
+        void swap(list<T>& l);
+    private:
+        void CreateHead()
+        {
+            _pHead = new Node;//data初始化为0
+            _pHead->_pNext = _pHead;
+            _pHead->_pPre = _pHead;
+        }
+        PNode _pHead;
+    };
+};
+int main()
+{
+
+    return 0;
+}
+
+
 //class Solution {
 //public:
 //    int myAtoi(string str) {
@@ -138,68 +300,68 @@ using namespace std;
 //        return s;
 //    }
 //};
-class Solution {
-public:
-    string reverseWords(string s) {
-        stack<char> s1;
-        int sz = s.size();
-        for (int i = 0; i < sz; i++)
-        {
-            if (s[i] == ' ')
-            {
-                i++;
-            }
-            int j = i;
-            int count = 0;
-            while (s[i] != ' ' && i != sz)//进栈
-            {
-                count++;
-                s1.push(s[i]);
-                i++;
-            }
-            //入栈完毕开始出栈
-            while (count--)
-            {
-                char ch = s1.top();
-                s1.pop();
-                s[j] = ch;
-                j++;
-            }
- 
-        }
-        return s;
-    }
-};
-
-int main()
-
-{
-
-    int array[] = { 1, 2, 3, 4, 0, 5, 6, 7, 8, 9 };
-
-    int n = sizeof(array) / sizeof(int);
-
-    list<int> mylist(array, array + n);
-
-    auto it = mylist.begin();
-
-    while (it != mylist.end())
-
-    {
-
-        if (*it != 0)
-
-            cout << *it << " ";
-
-        else
-
-            it = mylist.erase(it);
-
-        ++it;
-
-    }
-
-    return 0;
-
-}
+//class Solution {
+//public:
+//    string reverseWords(string s) {
+//        stack<char> s1;
+//        int sz = s.size();
+//        for (int i = 0; i < sz; i++)
+//        {
+//            if (s[i] == ' ')
+//            {
+//                i++;
+//            }
+//            int j = i;
+//            int count = 0;
+//            while (s[i] != ' ' && i != sz)//进栈
+//            {
+//                count++;
+//                s1.push(s[i]);
+//                i++;
+//            }
+//            //入栈完毕开始出栈
+//            while (count--)
+//            {
+//                char ch = s1.top();
+//                s1.pop();
+//                s[j] = ch;
+//                j++;
+//            }
+// 
+//        }
+//        return s;
+//    }
+//};
+//
+//int main()
+//
+//{
+//
+//    int array[] = { 1, 2, 3, 4, 0, 5, 6, 7, 8, 9 };
+//
+//    int n = sizeof(array) / sizeof(int);
+//
+//    list<int> mylist(array, array + n);
+//
+//    auto it = mylist.begin();
+//
+//    while (it != mylist.end())
+//
+//    {
+//
+//        if (*it != 0)
+//
+//            cout << *it << " ";
+//
+//        else
+//
+//            it = mylist.erase(it);
+//
+//        ++it;
+//
+//    }
+//
+//    return 0;
+//
+//}
 
