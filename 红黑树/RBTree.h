a6f175279fbeeb515b211a5e54b5536755cf26c4
simple-and-silly-matcha-c <1476303287@qq.com>
2024-07-25 22:46:49 +0800

    725

--- 红黑树/RBTree.h
+++ 红黑树/RBTree.h
@@ -36,6 +36,8 @@ public:
 		if (_root == nullptr)
 		{
 			_root = new Node(kv);
+
+			_root->_col = BLACK;
 			return true;
 		}
 
@@ -81,7 +83,9 @@ public:
 			//parent在左
 			if (parent == grandfather->_left)
 			{
-				if ()
+				Node* uncle = grandfather->_right;
+				//Node* uncle = parent->_right;//错误二
+				if (uncle && uncle->_col == RED)
 				{
 				//情景一：cur->红，parent->红，grandfather->黑，uncle存在且为红
 					//     g
@@ -89,25 +93,70 @@ public:
 					// c
 					// 
 					//解决：p，u改为黑，g改为红，最后g为红所以，要继续向上调整
+					parent->_col = uncle->_col = BLACK;
+					grandfather->_col = RED;
+					cur = grandfather;
+					parent = cur->_parent;
 				}
 				else
-				{
-				//情景二：cur->红，parent->红，grandfather->黑，uncle不存在/为黑
-					//     g
-					//   p   u
-					// c
-					// 
+				{					
+					if (cur == parent->_left)
+					{
+					//情景二：cur->红，parent->红，grandfather->黑，uncle不存在/为黑
+					    //     g
+					    //   p   u
+					    // c
+					    // 
 					// 解决：对g右单旋，p改为黑，g改为红，最后g为黑所以，直接break
-				//情景三：cur->红，parent->红，grandfather->黑，uncle不存在/为黑
-					//       g
-					//   p      u
-					//     c
-					// 解决：对p左单旋，后变为情景二。
+						RotateR(grandfather);
+						parent->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					else
+					{
+					//情景三：cur->红，parent->红，grandfather->黑，uncle不存在/为黑
+					    //       g
+				        //   p      u
+					    //     c
+				    // 解决：对p左单旋，后变为情景二。
+						RotateL(parent);
+						RotateR(grandfather);
+						cur->_col = BLACK;
+						grandfather->_col = RED;
+					}				
+					break;
 				}
 			}
 			else//情景大概反着来
 			{
+				//1  uncle
+				Node* uncle = grandfather->_left;//错误一
+				//Node* uncle = parent->_right;//错误一
+				if (uncle && uncle->_col == RED)
+				{
+					parent->_col = uncle->_col = BLACK;
+					grandfather->_col = RED;
+					cur = grandfather;
+					parent = cur->_parent;
+				}
 
+				else
+				{
+					if (cur == parent->_right)//2
+					{
+						RotateL(grandfather);
+						parent->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					else//3
+					{
+						RotateR(parent);
+						RotateL(grandfather);
+						cur->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					break;
+				}
 			}			
 		}
 		//最后
@@ -181,6 +230,69 @@ public:
 
 			subL->_parent = parentParent;
 		}
+	}
+	void InOrder()
+	{
+		_InOrder(_root);
+		cout << endl;
+	}
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+			return;
+		_InOrder(root->_left);
+		cout << root->_kv.first << " ";
+		_InOrder(root->_right);
+	}
+	bool Check(Node* root, int blacknum, const int refVal)
+	{
+		if (root == nullptr)
+		{
+			if (refVal != blacknum)
+			{
+				cout << "存在黑色节点数量不相等的路径" << endl;
+				return false;
+			}
+			return true;
+		}
+		if (root->_col == RED)
+		{
+			if (root->_parent->_col == RED)
+			{
+				cout << "有连续的红色节点" << endl;
+				return false;
+			}
+		}
+		if (root->_col == BLACK)
+		{
+			++blacknum;
+		}
+		return Check(root->_left, blacknum, refVal)
+			&& Check(root->_right, blacknum, refVal);
+
+	}
+	bool IsBalance()
+	{
+		 //1:是否存在 红-红 
+		//每条路径黑色结点是否相同个数
+		//最长的不超过最短的二倍
+		//根，叶子为黑
+		if (_root == nullptr)
+			return true;
+		if (_root->_col == RED)
+			return false;
+		int refVal = 0;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_col == BLACK)
+			{
+				++refVal;
+			}
+			cur = cur->_left;
+		}
+		int blacknum = 0;
+		return Check(_root, blacknum, refVal);
 	}
 private:
 	Node* _root = nullptr;
--- 红黑树/test.cpp
+++ 红黑树/test.cpp
@@ -6,8 +6,26 @@ using namespace std;
 #include"RBTree.h"
 int main()
 {
+	const int N = 10000;
+	vector<int> v;
+	v.reserve(N);
+	//srand(time(0));
+	for (size_t i = 0; i < N; i++)
+	{
+		v.push_back(rand());
+	}
+	size_t begin2 = clock();
 	RBTree<int, int> t;
-	t.Insert(make_pair(1, 1));
-	t.Insert(make_pair(2, 2));
+	for (auto e : v)
+	{
+		if (e == 26500)
+		{
+			int m = 0;
+		}
+		t.Insert(make_pair(e, e));
+		//cout << "Insert:" << e << "->" << t.IsBalance() << endl;
+	}
+	size_t end2 = clock();
+	cout << t.IsBalance() << endl;
 	return 0;
 }
