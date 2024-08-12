2b4d1c13e1be964d81d5bca7d385b55c3956a95f
simple-and-silly-matcha-c <1476303287@qq.com>
2024-08-12 22:57:53 +0800

    712

---
+++ 搜索 二叉树/test.cpp
@@ -0,0 +1,23 @@
+#include"BinarySearchTree.h"
+
+int main()
+{
+	int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+	BSTree<int> bt;
+	for (auto e : a)
+	{
+		bt.InsertR(e);
+	}
+	bt.InOrder();
+	//cout << bt.Find(3) << endl;
+	bt.EraseR(6);
+	bt.Erase(4);
+	bt.InOrder();
+	for (auto e : a)
+	{
+		bt.EraseR(e);
+		bt.InOrder();
+	}
+
+	return 0;
+}
