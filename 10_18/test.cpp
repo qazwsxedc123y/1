f736478ef054de1b2ffdbbce172668a07660c8df
simple-and-silly-matcha-c <1476303287@qq.com>
2024-10-19 10:07:14 +0800

    哈希的实现

--- 10_18/test.cpp
+++ 10_18/test.cpp
@@ -65,14 +65,156 @@ namespace open_address
 			_tables.resize(10);
 		}
 
-		bool Insert(const pair<K, V>& kv);
-		HashData<K, V>* Find(const K& key);
-		bool Erase(const K& key);
+		bool Insert(const pair<K, V>& kv)
+		{
+			if (Find(kv.first))
+				return false;
+
+			// 负载因子0.7就扩容
+			if (_n * 10 / _tables.size() >= 7)//扩容
+			{
+				size_t newSize = _tables.size() * 2;
+				HashTable<K, V, Hash> newHT;
+				newHT._tables.resize(newSize);
+				for (size_t i = 0; i < _tables.size(); i++)
+				{
+					if (_tables[i]._state == EXIST)
+					{
+						newHT.Insert(_tables[i]._kv);
+					}
+				}
+				_tables.swap(newHT._tables);
+			}
+			Hash hf;
+			// 线性探测
+			size_t hashi = hf(kv.first) % _tables.size();
+			while (_tables[hashi]._state == EXIST)
+			{
+				++hashi;
+				hashi %= _tables.size();
+			}
+
+			_tables[hashi]._kv = kv;
+			_tables[hashi]._state = EXIST;
+			++_n;
+
+			return true;
+		}
+		HashData<K, V>* Find(const K& key)
+		{
+			Hash hf;
+			size_t hashi = hf(key) % _tables.size();
+			while (_tables[hashi]._state != EMPTY)
+			{
+				if (_tables[hashi]._state != DELETE
+					&& _tables[hashi]._kv.first == key)
+					return &_tables[hashi];
+				++hashi;
+				hashi %= _tables.size();
+			}
+
+			//没有找到
+			return NULL;
+		}
+		bool Erase(const K& key)
+		{
+			HashData<K, V>* ret = Find(key);
+			if (ret)//存在
+			{
+				--_n;
+				ret->_state = DELETE;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		void Print()
+		{
+			for (size_t i = 0; i < _tables.size(); i++)
+			{
+				if (_tables[i]._state == EXIST)
+				{
+					cout << "[" << i << "]->" << _tables[i]._kv.first << ":" << _tables[i]._kv.second << endl;
+				}
+				else if (_tables[i]._state == EMPTY)
+				{
+					printf("[%zd]->\n", i);
+				}
+				else
+				{
+					printf("[%zd]->D\n", i);
+				}
+			}
+			cout << endl;
+		}
 
 	private:
 		vector<HashData<K, V>> _tables;
 		size_t _n = 0;  // 表中存储数据个数
 	};
+	void TestHT1()
+	{
+		HashTable<int, int> ht;
+		int a[] = { 4,14,24,34,5,7,1,6,10,56,11,35 };
+		for (auto e : a)
+		{
+			ht.Insert(make_pair(e, e));
+		}
+		ht.Insert(make_pair(-3, -3));
+		ht.Insert(make_pair(3, 3));
+		ht.Insert(make_pair(-3, -3));
+		ht.Print();
+		ht.Erase(3);
+		ht.Print();
+		if (ht.Find(3))
+		{
+			cout << "3存在" << endl;
+		}
+		else
+		{
+			cout << "3不存在" << endl;
+		}
+
+		ht.Insert(make_pair(3, 3));
+		ht.Insert(make_pair(23, 3));
+		ht.Print();
+	}
+	void TestHT2()
+	{
+		string arr[] = { "香蕉", "甜瓜","苹果", "西瓜", "苹果", "西瓜", "苹果", "苹果", "西瓜", "苹果", "香蕉", "苹果", "香蕉" };
+		//HashTable<string, int, HashFuncString> ht;
+		HashTable<string, int> ht;
+		for (auto& e : arr)
+		{
+			HashData<string, int>* ret = ht.Find(e);
+			if (ret)//存在
+			{
+				ret->_kv.second++;
+			}
+			else
+			{
+				ht.Insert(make_pair(e, 1));
+			}
+		}
+		ht.Print();
+
+		ht.Insert(make_pair("apple", 1));
+		ht.Insert(make_pair("sort", 1));
+
+		ht.Insert(make_pair("abc", 1));
+		ht.Insert(make_pair("acb", 1));
+		ht.Insert(make_pair("aad", 1));
+
+		ht.Print();
+	}
+}
+int main()
+{
+	open_address::TestHT1();
+	open_address::TestHT2();
+	return 0;
 }
 
 
