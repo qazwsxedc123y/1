bcba11e3dc661b34bbdab19c05fd5b19dfe74164
simple-and-silly-matcha-c <1476303287@qq.com>
2024-10-23 22:48:14 +0800

    23号力扣刷题3道中等题

--- 10_22/test.cpp
+++ 10_22/test.cpp
@@ -45,12 +45,104 @@ using namespace std;
 //        return v;
 //    }
 //};
+//class Solution {
+//public:
+//    long long countCompleteDayPairs(vector<int>& hours) {
+//        long long ret = 0;
+//        unordered_map<int, int> mp;
+//        for (auto& hour : hours)
+//        {
+//            int h = hour % 24;
+//            int grap = (24 - h) % 24;
+//            ret += mp[grap];
+//            mp[h]++;
+//        }
+//        return ret;
+//    }
+//};
+
+
+
+//const pair<int, string> valueSymbols[] = {
+//    {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
+//    {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
+//    {5, "V"},    {4, "IV"},   {1, "I"},
+//};
+//class Solution {
+//public:
+//    string intToRoman(int num) {
+//        string ret;
+//        for (auto& [n, r] : valueSymbols) {
+//            while (num >= n) {
+//                num -= n;
+//                ret += r;
+//            }
+//        }
+//        return ret;
+//    }
+//}; 
+ 
+
+//class Solution {
+//public:
+//    int threeSumClosest(vector<int>& nums, int target) {
+//        int n = nums.size();
+//        int best = 1e7;
+//        sort(nums.begin(), nums.end());
+//        auto update = [&](int cur)
+//            {
+//                if (abs(cur - target) < abs(best - target))
+//                {
+//                    best = cur;
+//                }
+//            };
+//        for (int i = 0; i < n; i++)
+//        {
+//            if (i > 0 && nums[i] == nums[i - 1])
+//            {
+//                continue;
+//            }
+//            int j = i + 1, k = n - 1;
+//            while (j < k)
+//            {
+//                int sum = nums[i] + nums[j] + nums[k];
+//                if (sum == target)
+//                {
+//                    return target;
+//                }
+//                update(sum);
+//                //调整边界情况
+//                if (sum > target)
+//                {
+//                    int k0 = k - 1;
+//                    while (j < k0 && nums[k0] == nums[k])
+//                    {
+//                        --k0;
+//                    }
+//                    k = k0;
+//                }
+//                else
+//                {
+//                    int j0 = j + 1;
+//                    while (j0 < k && nums[j] == nums[j0])
+//                    {
+//                        ++j0;
+//                    }
+//                    j = j0;
+//                }
+//            }
+//        }
+//        return best;
+//    }
+//};
 
 
 int main()
 {
+    vector<int> v{ -1,2,1,-4 };
+
     Solution s;
-    s.distributeCandies(60, 5);
+    s.threeSumClosest(v, 1);
     return 0;
 }
 
