c1c69f2ad4223ff67a894b9af4187bc4eb847d15
simple-and-silly-matcha-c <1476303287@qq.com>
2024-12-09 23:15:48 +0800

    补交另一种写法

--- 12_9/test.cpp
+++ 12_9/test.cpp
@@ -375,6 +375,40 @@ using namespace std;
 //    }
 //};
 
+class Solution {
+public:
+    int minNumberOfFrogs(string croakOfFrogs) {
+        int ret = 0;
+        string t = "croak";
+        int n = t.size();
+        vector<int> hash(n);// 用数组模拟哈希表
+        unordered_map<char, int> index;// 存储[x,x这个字符的对应的hash的下标]
+        for (int i = 0; i < n; i++)
+        {
+            index[t[i]] = i;
+        }
+        for (auto ch : croakOfFrogs)
+        {
+            if (ch == 'c')
+            {
+                if (hash[n - 1] != 0) hash[n - 1]--;
+                hash[0]++;
+            }
+            else
+            {
+                int i = index[ch];// 获取当前字符是什么
+                if (hash[i - 1] == 0) return -1;
+                hash[i - 1]--;
+                hash[i]++;
+            }
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (hash[i] != 0) return -1;
+        }
+        return hash[n - 1];
+    }
+};
+
 int main()
 {
     Solution s;
