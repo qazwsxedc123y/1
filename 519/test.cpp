6276252096795a8d7d3e7a1b816133c08abbd50e
qazwsxedc123y <1476303287@qq.com>
2025-05-19 23:16:18 +0800

    力扣每日一题

---
+++ 519/test.cpp
@@ -0,0 +1,24 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if (nums[0] + nums[1] <= nums[2])
+        {
+            return "none";
+        }
+        else if (nums[0] == nums[2])
+        {
+            return "equilateral";
+        }
+        else if (nums[0] == nums[1] || nums[1] == nums[2])
+        {
+            return "isosceles";
+        }
+        else
+        {
+            return "scalene";
+        }
+    }
+};
