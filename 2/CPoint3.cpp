0d535fdab07f38b778e845d496aac2c799b65a21
qazwsxedc123y <1476303287@qq.com>
2025-05-25 22:33:22 +0800

    小作品

--- 2/CPoint3.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "CPoint3.h"
-CPoint3::CPoint3(void) {}  // 删除多余右花括号
-
-CPoint3::~CPoint3(void) {} // 删除多余右花括号
-CPoint3::CPoint3(int x, int y, double z):CPoint2(x,y)
-{
-    this->z = z;  // 在构造函数体内赋值
-}
