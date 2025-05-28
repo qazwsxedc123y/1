45cadc3096fc6303b1d399cc0aa0a36e87d9910f
qazwsxedc123y <1476303287@qq.com>
2025-05-28 22:25:54 +0800

    刷题

--- test/test.cpp
+++ test/test.cpp
@@ -36,3 +36,48 @@ public:
         return n * (n + 1) / 2 - k * (k + 1) * m;
     }
 };
+
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == -2147483648 && divisor == -1) {
+            return 2147483647;
+        }
+        return dividend / divisor;
+    }
+};
+
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        // 使用栈来写
+        int maxret = 0;
+        stack<int> stk;
+        stk.push(-1);
+        // 为了方便( 用数字
+        int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '(')
+            {
+                stk.push(i);
+            }
+            else
+            {
+                stk.pop();
+                // 没有与之匹配的
+                if (stk.empty())
+                {
+                    stk.push(i);
+                }
+                else
+                {
+                    maxret = max(maxret, i - stk.top());
+                }
+            }
+        }
+        return maxret;
+    }
+};
