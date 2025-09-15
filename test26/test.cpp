83f647a5b57dbd3a3697526028000d4d08e5ceed
qazwsxedc123y <1476303287@qq.com>
2025-09-15 16:06:55 +0800

    两道题，二分代码需要重新学习

--- test26/test.cpp
+++ test26/test.cpp
@@ -621,3 +621,87 @@ using namespace std;
 //        return f[target];
 //    }
 //};
+
+
+//class Solution {
+//public:
+//    int canBeTypedWords(string text, string brokenLetters) {
+//        unordered_set<char> hash;
+//        for (auto e : brokenLetters)
+//            hash.insert(e);
+//
+//        int ret = 0;
+//        bool flag = true;
+//        for (auto ch : text)
+//        {
+//            if (ch == ' ')
+//            {
+//                if (flag) ret++;
+//                flag = true;
+//            }
+//
+//            else if (hash.find(ch) != hash.end())
+//            {
+//                flag = false;
+//            }
+//        }
+//
+//        // 处理末尾单词
+//        if (flag) ret++;
+//
+//        return ret;
+//    }
+//};
+
+
+class Solution {
+public:
+    int binary_l(vector<int>& nums, int target) // 第一个大于等于 target 的位置
+    {
+        int left = 0, right = nums.size() - 1;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    int binary_r(vector<int>& nums, int target) // 第一个大于 target 的位置
+    {
+        int left = 0, right = nums.size();
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.empty()) return { -1, -1 };
+
+        int left_idx = binary_l(nums, target);
+        // 检查是否找到target
+        if (nums[left_idx] != target) {
+            return { -1, -1 };
+        }
+
+        int right_idx = binary_r(nums, target) - 1;
+        return { left_idx, right_idx };
+    }
+};
