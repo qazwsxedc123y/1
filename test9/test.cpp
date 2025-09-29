ee035a6a2394e7fb6c7576cdf317f3b699f16317
qazwsxedc123y <1476303287@qq.com>
2025-09-29 22:16:39 +0800

    每日一题。dp

--- test9/test.cpp
+++ test9/test.cpp
@@ -209,6 +209,28 @@ using namespace std;
 //    }
 //};
 
+
+//class Solution {
+//public:
+//    int minScoreTriangulation(vector<int>& values) {
+//        int n = values.size();
+//        vector f(n, vector<int>(n));
+//        for (int i = n - 3; i >= 0; i--)
+//        {
+//            for (int j = i + 2; j < n; j++)
+//            {
+//                f[i][j] = INT_MAX;
+//                for (int k = i + 1; k < j; k++)
+//                {
+//                    f[i][j] = min(f[i][j], f[i][k] + f[k][j] + values[i] * values[j] * values[k]);
+//                }
+//            }
+//        }
+//        return f[0][n - 1];
+//    }
+//};
+
+
 int main()
 {
     //Solution s;
