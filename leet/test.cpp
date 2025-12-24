1668ab0ccd676f3308fdb76aab196b281694aa2f
qazwsxedc123y <1476303287@qq.com>
2025-12-24 19:38:28 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -2,100 +2,24 @@
 
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int ret = 0;
-        // 左右两边统计，分别统计至少几个糖果才可以满足
-        int n = ratings.size();
-        vector<int> dp(n, 1);
-        for (int i = 1; i < n; i++)
-        {
-            if (ratings[i] > ratings[i - 1])
-            {
-                dp[i] = dp[i - 1] + 1;
-            }
-        }
+    void rotate(vector<vector<int>>& matrix) {
+        // 水平 + 转置
+        int n = matrix.size();
 
-        for (int i = n - 2; i >= 0; i--)
+        for (int i = 0; i < n / 2; i++)
         {
-            if (ratings[i] > ratings[i + 1])
+            for (int j = 0; j < n; j++)
             {
-                dp[i] = max(dp[i], dp[i + 1] + 1);
+                swap(matrix[i][j], matrix[n - 1 - i][j]);
             }
         }
 
         for (int i = 0; i < n; i++)
         {
-            ret += dp[i];
-        }
-        return ret;
-    }
-};
-
-class Solution {
-public:
-    int candy(vector<int>& ratings) {
-        int ret = 0;
-        // 左右两边统计，分别统计至少几个糖果才可以满足
-        int n = ratings.size();
-        vector<int> f(n, 1); // 左 -》 右
-        vector<int> g(n, 1); // 右 -》 左
-        for (int i = 1; i < n; i++)
-        {
-            if (ratings[i] > ratings[i - 1])
+            for (int j = 0; j <= i; j++)
             {
-                f[i] = f[i - 1] + 1;
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-
-        for (int i = n - 2; i >= 0; i--)
-        {
-            if (ratings[i] > ratings[i + 1])
-            {
-                g[i] = g[i + 1] + 1;
-            }
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            ret += max(f[i], g[i]);
-        }
-        return ret;
-    }
-};
-
-
-class Solution {
-public:
-    int candy(vector<int>& ratings) {
-        int ret = 0;
-        // 连续上/下台阶次数？
-        // 单次下一次不计算
-        int n = ratings.size();
-        ret += n;
-        // 统计连续上下台阶次数
-        // 但是如果是峰值，且两边连续大于1有问题
-        for (int i = 1; i < n; i++)
-        {
-            int count = 0;
-            if (ratings[i] > ratings[i - 1]) // 统计上
-            {
-                while (i < n && ratings[i] > ratings[i - 1])
-                {
-                    count++;
-                    i++;
-                    ret += count;
-                }
-            }
-            else
-            {
-                while (i < n && ratings[i] < ratings[i - 1])
-                {
-                    count++;
-                    i++;
-                    ret += count;
-                }
-            }
-        }
-        return ret;
     }
 };
