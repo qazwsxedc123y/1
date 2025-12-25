e68c8e828919ebcc5d4731ce1f238a132b2057fd
qazwsxedc123y <1476303287@qq.com>
2025-12-25 10:07:20 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -1,25 +1,42 @@
 #define  _CRT_SECURE_NO_WARNINGS
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        // Ë®Æ½ + ×ªÖÃ
-        int n = matrix.size();
-
-        for (int i = 0; i < n / 2; i++)
+    ListNode* _head;
+    int sz = 0;
+    Solution(ListNode* head) {
+        _head = head;
+        while (head != nullptr)
         {
-            for (int j = 0; j < n; j++)
-            {
-                swap(matrix[i][j], matrix[n - 1 - i][j]);
-            }
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j <= i; j++)
-            {
-                swap(matrix[i][j], matrix[j][i]);
-            }
+            sz++;
+            head = head->next;
         }
     }
+
+    int getRandom() {
+        int index = rand() % sz;
+        ListNode* tail = _head;
+        while (index)
+        {
+            tail = tail->next;
+            index--;
+        }
+        return tail->val;
+    }
 };
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
