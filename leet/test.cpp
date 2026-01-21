d3091ea9984c6691a702275c125fc03c1a652545
qazwsxedc123y <1476303287@qq.com>
2026-01-21 20:08:42 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -1,41 +1,61 @@
-class MyStack {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    queue<int> q1, q2;
-    // q2入  q1出
-    MyStack() {
-
-    }
-
-    void push(int x) {
-        q2.push(x);
-        while (!q1.empty())
+    void reorderList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
         {
-            q2.push(q1.front());
-            q1.pop();
+            return;
         }
-        swap(q1, q2);
+        // 先将后半段进行翻转
+        ListNode* mid = mid_node(head);
+        ListNode* tmp = mid;
+        mid = reverse_node(mid->next);
+        tmp->next = nullptr;
+        // 将进行按序插入
+        sort_node(head, mid);
     }
+    void sort_node(ListNode* node1, ListNode* node2)
+    {
+        ListNode* node1_tmp = node1, * node2_tmp = node2;
+        while (node1 != nullptr && node2 != nullptr)
+        {
+            node1_tmp = node1->next;
+            node2_tmp = node2->next;
 
-    int pop() {
-        int r = q1.front();
-        q1.pop();
-        return r;
+            // 修改
+            node1->next = node2;
+            node1 = node1_tmp;
+
+            node2->next = node1;
+            node2 = node2_tmp;
+        }
     }
-
-    int top() {
-        return q1.front();
+    ListNode* mid_node(ListNode* head)
+    {
+        ListNode* fast = head, * slow = head;
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
     }
-
-    bool empty() {
-        return q1.empty();
+    ListNode* reverse_node(ListNode* head)
+    {
+        // 将head为头，然后的全部翻转
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* ret = reverse_node(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return ret;
     }
 };
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
