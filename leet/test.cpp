faf209fa90a8c9d618d1920dde9de36ff7e6159f
qazwsxedc123y <1476303287@qq.com>
2026-01-26 21:42:10 +0800

    多表查询

--- leet/test.cpp
+++ leet/test.cpp
@@ -2,29 +2,17 @@
 #include <vector>
 using namespace std;
 
-class Solution {
-public:
-    void hanota(vector<int>& A, vector<int>& B, vector<int>& C) {
-        // 将A盘的n个，借助B盘，放到C盘
-        dfs(A, B, C, A.size());
-    }
-    void dfs(vector<int>& A, vector<int>& B, vector<int>& C, int n)
-    {
-        // 将A盘的n个，借助B盘，放到C盘
-        if (n == 1)
-        {
-            C.push_back(A.back());
-            A.pop_back();
-            return;
-        }
-        // 主要思路，先将A的n-1个移到B上，再将最后一个移到C上
-        // 将B上的n-1个移到C上，   
-        dfs(A, C, B, n - 1);
-        C.push_back(A.back());
-        A.pop_back();
-        dfs(B, A, C, n - 1);
-    }
-};
+// select employees.emp_no, salary from employees, salaries where employees.emp_no = salaries.emp_no 
+// and hire_date = from_date order by emp_no desc;
+
+//select emp_no from employees
+//where emp_no not in(select emp_no from dept_manager);
+
+//select emp_no, manager from
+//(select dept_emp.emp_no emp_no, dept_manager.emp_no manager from dept_emp, dept_manager
+//    where dept_emp.dept_no = dept_manager.dept_no) t1 where emp_no <> manager;
+
+
 int main()
 {
     vector<int> ans{ 4,6,7,7 };
