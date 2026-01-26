9268252227abb0d78ab22c5d7205a5523eb4848e
qazwsxedc123y <1476303287@qq.com>
2026-01-26 20:57:40 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -2,7 +2,29 @@
 #include <vector>
 using namespace std;
 
-// select id, length(string) - length(replace(string, ',', '')) from strings;
+class Solution {
+public:
+    void hanota(vector<int>& A, vector<int>& B, vector<int>& C) {
+        // 将A盘的n个，借助B盘，放到C盘
+        dfs(A, B, C, A.size());
+    }
+    void dfs(vector<int>& A, vector<int>& B, vector<int>& C, int n)
+    {
+        // 将A盘的n个，借助B盘，放到C盘
+        if (n == 1)
+        {
+            C.push_back(A.back());
+            A.pop_back();
+            return;
+        }
+        // 主要思路，先将A的n-1个移到B上，再将最后一个移到C上
+        // 将B上的n-1个移到C上，   
+        dfs(A, C, B, n - 1);
+        C.push_back(A.back());
+        A.pop_back();
+        dfs(B, A, C, n - 1);
+    }
+};
 int main()
 {
     vector<int> ans{ 4,6,7,7 };
