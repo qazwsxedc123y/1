2b515c8f166c7e93ff6e16c62303a1dbbf7f39c0
qazwsxedc123y <1476303287@qq.com>
2026-02-06 17:01:49 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -4,50 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> v(m + 1, vector<int>(n + 1));
-
-        for (int i = 0; i < m; i++) v[i][0] = 101;
-        for (int i = 0; i < n; i++) v[0][i] = 101;
-        v[0][0] = v[0][1] = v[1][0] = 0;
-
-        for (int i = 1; i <= m; i++)
+    int firstUniqChar(string s) {
+        int cnt[32];
+        for (int i = 0; i < s.size(); i++)
         {
-            for (int j = 1; j <= n; j++)
-            {
-                v[i][j] = grid[i - 1][j - 1] + min(v[i - 1][j], v[i][j - 1]);
-                cout << v[i][j] << " ";
-            }
-            cout << endl;
+            cnt[s[i] - 'a']++;
         }
-
-        return v[m][n];
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (cnt[s[i] - 'a'] == 1) return i;
+        }
+        return -1;
     }
 };
-
-class Solution {
-private:
-    vector<vector<int>> v;
-public:
-    int mem(int i, int j, vector<vector<int>>& grid)
-    {
-        if (i < 0 || j < 0) return 1e5 + 7;
-
-        if (v[i][j] >= 0) return v[i][j];
-
-        v[i][j] = grid[i][j] + min(mem(i - 1, j, grid), mem(i, j - 1, grid));
-        return v[i][j];
-    }
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-
-        v.resize(m, vector<int>(n, -1));
-        v[0][0] = grid[0][0];
-        return mem(m - 1, n - 1, grid);
-    }
-};
-
 // grid = [[1,3,1],[1,5,1],[4,2,1]]
 int main()
 {
