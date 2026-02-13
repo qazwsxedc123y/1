bdea8db5d70ffe859f6ecaf8d776620b8ef02a41
qazwsxedc123y <1476303287@qq.com>
2026-02-13 21:56:00 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -7,75 +7,70 @@ using namespace std;
 
 //class Solution {
 //public:
-//    int longestValidParentheses(string s) {
-//        // 使用栈
-//        int ret = 0, ans = -1;
-//        stack<int> st; // 只入 ( 的
-//        st.push(-1);
-//        int n = s.size();
-//        for (int i = 0; i < n; i++)
+//    int myAtoi(string s) {
+//        long long ret = 0;
+//        int i = 0, n = s.size();
+//        //去除前置空格
+//        while (i < n)
 //        {
-//            if (s[i] == '(') // 直接入栈
-//            {
-//                st.push(i);
-//            }
-//            else // 看看是否匹配，进行出栈
-//            {
-//                st.pop();
-//                if (st.empty())
-//                {
-//                    // 去除的是 -1，栈内无匹配
-//                    st.push(-1);
-//                    ans = i;
-//                }
-//                else
-//                {
-//                    ret = max(ret, i - ans);
-//                }
-//            }
+//            if (s[i] != ' ')
+//                break;
+//            i++;
+//        }
+//        int flag = 1;
+//        if (s[i] == '-')
+//        {
+//            flag = -1;
+//            i++;
+//        }
+//        else if (s[i] == '+') i++;
+//        while (i < n)
+//        {
+//            char ch = s[i];
+//            if (ch >= '0' && ch <= '9')
+//                ret = ret * 10 + ch - '0';
+//            else
+//                break;
+//            i++;
+//            if (ret * flag > INT_MAX)
+//                return INT_MAX;
+//            else if (ret * flag < INT_MIN)
+//                return INT_MIN;
 //        }
 //
-//        return ret;
+//        return ret * flag;
 //    }
 //};
 
 
 class Solution {
 public:
-    int longestValidParentheses(string s) {
-        // 动态规划
-        // dp[i]表示以下标 i 字符结尾的最长有效括号的长度
-        int ret = 0, n = s.size();
-        vector<int> dp(n, 0);
-        // 初始化，dp[0] = 0;
-        for (int i = 1; i < n; i++)
+    int minDistance(string word1, string word2) {
+        // 一定要变为相同长度
+        // A插入，B插入，替换
+        int n = word1.size(), m = word2.size();
+        if (n * m == 0) return n + m;
+        int dp[n + 1][m + 1]; // dp[i][j] 表示word1 到 i 位置转换成 word2 到 j 位置需要最少步数
+        // 初始化
+        for (int i = 0; i <= n; i++) dp[i][0] = i;
+        for (int i = 0; i <= m; i++) dp[0][i] = i;
+
+        for (int i = 1; i <= n; i++)
         {
-            if (s[i] == ')')
+            for (int j = 1; j <= m; j++)
             {
-                if (s[i - 1] == '(')
-                {
-                    if (i >= 2) dp[i] = dp[i - 2] + 2;
-                    else dp[i] = 2;
-                }
-                else
-                {
-                    // "()(())"
-                    // 前有dp[i-1]个配对
-                    int ans = i - dp[i - 1] - 1; // 看与s[ans]是否配对
-                    // "(())(())"
-                    if (ans >= 0 && s[ans] == '(')
-                    {
-                        if (ans - 1 >= 0 && dp[ans - 1] != 0) dp[i] = dp[ans - 1] + dp[i - 1] + 2;
-                        else dp[i] = dp[i - 1] + 2;
-                    }
-                }
-                ret = max(ret, dp[i]);
+                // 替换 dp[i-1][j-1]
+                // A插入 dp[i][j-1]
+                // B插入 dp[i-1][j]
+                if (word1[i - 1] != word2[j - 1]) dp[i][j] = min(min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
+                else dp[i][j] = dp[i - 1][j - 1];
+                cout << dp[i][j] << " ";
             }
+            cout << endl;
         }
-        return ret;
+        return dp[n][m];
     }
 };
-
 
 int main()
 {
