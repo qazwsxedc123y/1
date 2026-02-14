73428f7d308a9b2f784ea21e0ad7a30ff0c6878f
qazwsxedc123y <1476303287@qq.com>
2026-02-14 23:48:46 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -5,72 +5,58 @@
 #include <algorithm>
 using namespace std;
 
-//class Solution {
-//public:
-//    int myAtoi(string s) {
-//        long long ret = 0;
-//        int i = 0, n = s.size();
-//        //去除前置空格
-//        while (i < n)
-//        {
-//            if (s[i] != ' ')
-//                break;
-//            i++;
-//        }
-//        int flag = 1;
-//        if (s[i] == '-')
-//        {
-//            flag = -1;
-//            i++;
-//        }
-//        else if (s[i] == '+') i++;
-//        while (i < n)
-//        {
-//            char ch = s[i];
-//            if (ch >= '0' && ch <= '9')
-//                ret = ret * 10 + ch - '0';
-//            else
-//                break;
-//            i++;
-//            if (ret * flag > INT_MAX)
-//                return INT_MAX;
-//            else if (ret * flag < INT_MIN)
-//                return INT_MIN;
-//        }
-//
-//        return ret * flag;
-//    }
-//};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int d1 = 0, d2 = 0;
+        TreeNode* left = root->left, * right = root->right;
+
+        while (left)
+        {
+            d1++;
+            left = left->left;
+        }
+        while (right)
+        {
+            d2++;
+            right = right->right;
+        }
+        if (d1 == d2) return pow(2, d1 + 1) - 1;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
 
 
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        // 一定要变为相同长度
-        // A插入，B插入，替换
-        int n = word1.size(), m = word2.size();
-        if (n * m == 0) return n + m;
-        int dp[n + 1][m + 1]; // dp[i][j] 表示word1 到 i 位置转换成 word2 到 j 位置需要最少步数
-        // 初始化
-        for (int i = 0; i <= n; i++) dp[i][0] = i;
-        for (int i = 0; i <= m; i++) dp[0][i] = i;
-
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                // 替换 dp[i-1][j-1]
-                // A插入 dp[i][j-1]
-                // B插入 dp[i-1][j]
-                if (word1[i - 1] != word2[j - 1]) dp[i][j] = min(min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
-                else dp[i][j] = dp[i - 1][j - 1];
-                cout << dp[i][j] << " ";
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int left = -1, right = m * n;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            int x = matrix[mid / n][mid % n];
+            if (x == target) {
+                return true;
             }
-            cout << endl;
+            (x < target ? left : right) = mid;
         }
-        return dp[n][m];
+        return false;
     }
 };
+
 
 int main()
 {
