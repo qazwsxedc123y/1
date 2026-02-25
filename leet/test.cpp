c6194204ecefdd534fb688b1fb078cad86626106
qazwsxedc123y <1476303287@qq.com>
2026-02-25 21:25:21 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -8,70 +8,103 @@ using namespace std;
 
 //class Solution {
 //public:
-//    int lengthOfLIS(vector<int>& nums) {
-//        // 使用动态规划
-//        int n = nums.size();
-//        vector<int> dp(n, 1); // dp[i]表示区间[0,i]之间最大递增子序列长度
-//        for (int i = 1; i < n; i++)
+//    int findDuplicate(vector<int>& nums) {
+//        //
+//        int sz = nums.size(); // 范围 1 ~ sz-1
+//        // 只有一个数是重复的
+//        // 按位与
+//        int ret = 0;
+//        for (int i = 0; i < 32; i++)
 //        {
-//            for (int j = 0; j < i; j++)
+//            int x = 0, y = 0;
+//            for (int j = 0; j < sz; j++)
 //            {
-//                if (nums[i] > nums[j])
-//                {
-//                    dp[i] = max(dp[i], dp[j] + 1);
-//                }
+//                // 计算当前为是什么
+//                if ((1 << i) & nums[j]) x++;
+//
+//                if (j >= 1 && (j & (1 << i))) y++;
+//            }
+//            if (x > y)
+//            {
+//                // 那么重复的那个数该位为1
+//                ret |= (1 << i);
 //            }
 //        }
-//        int ret = 0;
-//        for (int i = 0; i < n; i++) ret = max(ret, dp[i]);
+//
 //        return ret;
+//    }
+//};
+
+
+//class Solution {
+//public:
+//    int findDuplicate(vector<int>& nums) {
+//        int slow = 0, fast = 0;
+//        do {
+//            slow = nums[slow];
+//            fast = nums[nums[fast]];
+//        } while (slow != fast);
+//        slow = 0;
+//        while (slow != fast) {
+//            slow = nums[slow];
+//            fast = nums[fast];
+//        }
+//        return slow;
 //    }
 //};
 
 
 class Solution {
 public:
-    int findNumberOfLIS(vector<int>& nums) {
-        // 使用动态规划
-        int n = nums.size();
-        vector<int> dp(n, 1); // dp[i]表示区间[0,i]之间最大递增子序列长度
-        vector<int> count(n, 1); // count[i]表示区间[0,i]之间最大递增子序列长度的个数
-        int ret = 1, max_len = 1;
-        for (int i = 1; i < n; i++)
+    string longestPalindrome(string s) {
+        // 动态规划
+        int n = s.size();
+        if (n < 2) return s;
+
+        vector<vector<bool>> dp(n, vector<bool>(n)); // dp[i][j]表示下标[i,j]区间是否为回文
+        // 首先长度为1的肯定是回文
+        for (int i = 0; i < n; i++) dp[i][i] = true;
+
+        int begin = 0, max_len = 1;
+
+        // 查看长度大于等于2的
+        for (int len = 2; len <= n; len++)
         {
-            for (int j = 0; j < i; j++)
+            for (int i = 0; i < n; i++) // 左
             {
-                if (nums[i] > nums[j])
+                int j = i + len - 1; // 右
+                if (j < n && s[i] == s[j])
                 {
-                    if (dp[i] == dp[j] + 1) count[i] += count[j];
-                    else if (dp[i] < dp[j] + 1) count[i] = count[j];
-                    dp[i] = max(dp[i], dp[j] + 1);
+                    // 特殊处理2，因为如果长度为2，那么dp[i+1][j-1]就会变为dp[j][i]
+                    if (len == 2) dp[i][j] = true;
+                    else if (dp[i + 1][j - 1])
+                    {
+                        dp[i][j] = true;
+                    }
+                    if (dp[i][j] && len > max_len)
+                    {
+                        max_len = len;
+                        begin = i;
+                    }
                 }
             }
-            if (max_len < dp[i])
-            {
-                max_len = dp[i];
-                ret = count[i];
-            }
-            else if (max_len == dp[i])
-            {
-                ret += count[i];
-            }
         }
-        return ret;
+
+        return s.substr(begin, max_len);
     }
 };
 
 
 int main()
 {
-    vector<vector<int>> v;
-    v.push_back({ 1,1,1 });
-    v.push_back({ 1,0,1 });
-    v.push_back({ 1,1,1 });
+    //vector<vector<int>> v;
+    //v.push_back({ 1,1,1 });
+    //v.push_back({ 1,0,1 });
+    //v.push_back({ 1,1,1 });
     // [1,1,1],[1,0,1],[1,1,1]
     Solution s;
-    s.setZeroes(v);
+    int x = 100;
+    // s.setZeroes(v);
     return 0;
 }
 
@@ -85,3 +118,32 @@ int main()
  * bool param_5 = obj->isEmpty();
  * bool param_6 = obj->isFull();
  */
+
+
+//void checkEndianness() {
+//    int num = 1;  // 二进制：0x00000001
+//
+//    // 将int指针转为char指针，只取第一个字节
+//    char* ptr = (char*)&num;
+//
+//    if (*ptr == 1) {
+//        cout << "小端模式 (Little Endian)" << endl;
+//        cout << "说明：低位字节(0x01)存储在低地址" << endl;
+//    }
+//    else {
+//        cout << "大端模式 (Big Endian)" << endl;
+//        cout << "说明：高位字节(0x00)存储在低地址" << endl;
+//    }
+//
+//    // 打印内存布局
+//    cout << "\n内存中的字节顺序：" << endl;
+//    for (int i = 0; i < sizeof(int); i++) {
+//        printf("地址 %p : 0x%02x\n", ptr + i, (unsigned char)ptr[i]);
+//    }
+//}
+//
+//int main() {
+//    cout << "=== 检测系统大小端 ===" << endl;
+//    checkEndianness();
+//    return 0;
+//}
