95d01b23d4a80a5ddc0961dcba03c872699032c1
qazwsxedc123y <1476303287@qq.com>
2026-02-26 21:55:21 +0800

    每日一题

--- leet/test.cpp
+++ leet/test.cpp
@@ -8,91 +8,41 @@ using namespace std;
 
 //class Solution {
 //public:
-//    int findDuplicate(vector<int>& nums) {
-//        //
-//        int sz = nums.size(); // 范围 1 ~ sz-1
-//        // 只有一个数是重复的
-//        // 按位与
-//        int ret = 0;
-//        for (int i = 0; i < 32; i++)
+//    string multiply(string num1, string num2) {
+//        // 先乘，然后再考虑进位
+//        int n = num1.size(), m = num2.size();
+//        reverse(num1.begin(), num1.end());
+//        reverse(num2.begin(), num2.end());
+//        vector<int> tmp(n + m);
+//        // 最多n + m位
+//        for (int i = 0; i < n; i++)
 //        {
-//            int x = 0, y = 0;
-//            for (int j = 0; j < sz; j++)
+//            for (int j = 0; j < m; j++)
 //            {
-//                // 计算当前为是什么
-//                if ((1 << i) & nums[j]) x++;
-//
-//                if (j >= 1 && (j & (1 << i))) y++;
-//            }
-//            if (x > y)
-//            {
-//                // 那么重复的那个数该位为1
-//                ret |= (1 << i);
+//                tmp[i + j] += ((num1[i] - '0') * (num2[j] - '0'));
 //            }
 //        }
+//
+//        //for(int i = 0; i < n+m; i++) cout << tmp[i] << " ";
+//
+//        // 调整进位
+//        int array = 0;
+//        string ret;
+//        for (int i = 0; i < n + m; i++)
+//        {
+//            tmp[i] += array;
+//            ret.push_back(tmp[i] % 10 + '0');
+//            array = tmp[i] / 10;
+//        }
+//        // 处理前导0
+//        while (ret.size() > 1 && ret.back() == '0') ret.pop_back();
+//
+//        reverse(ret.begin(), ret.end());
 //
 //        return ret;
+//
 //    }
 //};
-
-
-//class Solution {
-//public:
-//    int findDuplicate(vector<int>& nums) {
-//        int slow = 0, fast = 0;
-//        do {
-//            slow = nums[slow];
-//            fast = nums[nums[fast]];
-//        } while (slow != fast);
-//        slow = 0;
-//        while (slow != fast) {
-//            slow = nums[slow];
-//            fast = nums[fast];
-//        }
-//        return slow;
-//    }
-//};
-
-
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        // 动态规划
-        int n = s.size();
-        if (n < 2) return s;
-
-        vector<vector<bool>> dp(n, vector<bool>(n)); // dp[i][j]表示下标[i,j]区间是否为回文
-        // 首先长度为1的肯定是回文
-        for (int i = 0; i < n; i++) dp[i][i] = true;
-
-        int begin = 0, max_len = 1;
-
-        // 查看长度大于等于2的
-        for (int len = 2; len <= n; len++)
-        {
-            for (int i = 0; i < n; i++) // 左
-            {
-                int j = i + len - 1; // 右
-                if (j < n && s[i] == s[j])
-                {
-                    // 特殊处理2，因为如果长度为2，那么dp[i+1][j-1]就会变为dp[j][i]
-                    if (len == 2) dp[i][j] = true;
-                    else if (dp[i + 1][j - 1])
-                    {
-                        dp[i][j] = true;
-                    }
-                    if (dp[i][j] && len > max_len)
-                    {
-                        max_len = len;
-                        begin = i;
-                    }
-                }
-            }
-        }
-
-        return s.substr(begin, max_len);
-    }
-};
 
 
 int main()
