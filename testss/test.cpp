7a35bba00d602403b4cc4f70bac4583ffed26ab3
qazwsxedc123y <1476303287@qq.com>
2026-03-18 17:17:07 +0800

    每日一题

--- testss/test.cpp
+++ testss/test.cpp
@@ -10,55 +10,43 @@ using namespace std;
 #include <vector>
 class Solution {
 public:
-    string decodeString(string s) {
-        // 感觉可以用栈
-        int n = s.size(), i = 0;
-        stack<int> st1;
-        stack<string> st2;
-        st2.push(""); // 避免为空的情况，即全为普通串
-        while (i < n)
+    string addBinary(string a, string b) {
+        // 模拟
+        int n1 = a.size(), n2 = b.size();
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        int n = max(n1, n2);
+        int carry = 0;
+        string ret;
+        for (int i = 0; i < n; i++)
         {
-            cout << i << " ";
-            if (s[i] == '[')
-            {
-                i++;
-                string tmp;
-                while (s[i] >= 'a' && s[i] <= 'z')
-                {
-                    tmp += s[i++];
-                }
-                st2.push(tmp);
-            }
-            else if (s[i] == ']')
-            {
-                // 统计，然后入栈
-                int ans = st1.top();
-                st1.pop();
-                string tmp = st2.top();
-                st2.pop();
-                while (ans)
-                {
-                    st2.top() += tmp;
-                    ans--;
-                }
-                i++;
-            }
-            else if (s[i] >= '0' && s[i] <= '9')
-            {
-                int ans = 0;
-                while (s[i] >= '0' && s[i] <= '9')
-                {
-                    ans = ans * 10 + s[i] - '0';
-                    i++;
-                }
-                st1.push(ans);
-            }
-            else
-            {
-                st2.top() += s[i++];
-            }
+            int t1 = (i < n1) ? a[i] - '0' : 0;
+            int t2 = (i < n2) ? b[i] - '0' : 0;
+            int sum = t1 + t2 + carry;
+            carry = sum / 2;
+            sum %= 2;
+            ret.push_back(sum + '0');
         }
-        return st2.top();
+        if (carry) ret.push_back(carry + '0');
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        // 直接使用动态规划
+        // 优化，使用一个数组
+        int n = nums.size();
+        int f[n + 1]; // f[i] 表示第i个屋子抢劫时的最大金额
+        f[0] = 0, f[1] = nums[0];
+        for (int i = 2; i <= n; i++)
+        {
+            f[i] = max(f[i - 1], f[i - 2] + nums[i - 1]);
+        }
+        return f[n];
     }
 };
 int main()
