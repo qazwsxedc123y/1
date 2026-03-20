ee6866d9b3bc2011064c9694ed457ae0ea1b5159
qazwsxedc123y <1476303287@qq.com>
2026-03-20 22:37:57 +0800

    每日一题

--- testss/test.cpp
+++ testss/test.cpp
@@ -3,52 +3,67 @@
 using namespace std;
 // bugTest.cpp: 定义应用程序的入口点。
 //
-
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    string addBinary(string a, string b) {
-        // 模拟
-        int n1 = a.size(), n2 = b.size();
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-        int n = max(n1, n2);
-        int carry = 0;
-        string ret;
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int d1 = 0, d2 = 0;
+        TreeNode* left = root->left, * right = root->right;
+
+        while (left)
+        {
+            d1++;
+            left = left->left;
+        }
+        while (right)
+        {
+            d2++;
+            right = right->right;
+        }
+        if (d1 == d2) return pow(2, d1 + 1) - 1;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int x = -1, y = 0;
         for (int i = 0; i < n; i++)
         {
-            int t1 = (i < n1) ? a[i] - '0' : 0;
-            int t2 = (i < n2) ? b[i] - '0' : 0;
-            int sum = t1 + t2 + carry;
-            carry = sum / 2;
-            sum %= 2;
-            ret.push_back(sum + '0');
+            if (matrix[i][0] > target)
+            {
+                // 必在上一行
+                x = i - 1;
+                break;
+            }
+            else if (matrix[i][0] == target) return true;
         }
-        if (carry) ret.push_back(carry + '0');
-        reverse(ret.begin(), ret.end());
-        return ret;
-    }
-};
-
-
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        // 直接使用动态规划
-        // 优化，使用一个数组
-        int n = nums.size();
-        int f[n + 1]; // f[i] 表示第i个屋子抢劫时的最大金额
-        f[0] = 0, f[1] = nums[0];
-        for (int i = 2; i <= n; i++)
+        if (x == -1) x = n - 1;
+        cout << x << endl;
+        for (int i = 0; i < m; i++)
         {
-            f[i] = max(f[i - 1], f[i - 2] + nums[i - 1]);
+            if (matrix[x][i] == target) return true;
         }
-        return f[n];
+
+        return false;
     }
 };
+
 int main()
 {
 	cout << (int)'6' << endl;
