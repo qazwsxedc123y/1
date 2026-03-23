01dc4f1809ec000758cc0c62663510531f22c948
qazwsxedc123y <1476303287@qq.com>
2026-03-23 23:13:20 +0800

    每日一题

--- testss/test.cpp
+++ testss/test.cpp
@@ -2,68 +2,50 @@
 #include <iostream>
 using namespace std;
 // bugTest.cpp: 定义应用程序的入口点。
+// 
+// 
+//class Solution {
+//public:
+//    int coinChange(vector<int>& coins, int amount) {
+//        // 背包问题
+//        int n = coins.size();
+//        // dp[i][j] 表示从前i种硬币凑齐j元所使用的最少数量
+//        int dp[14][10005];
+//        memset(dp, 0x3f, sizeof(dp));
+//        for (int i = 0; i <= n; i++) dp[i][0] = 0;
 //
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int countNodes(TreeNode* root) {
-        if (root == nullptr) return 0;
-        int d1 = 0, d2 = 0;
-        TreeNode* left = root->left, * right = root->right;
-
-        while (left)
-        {
-            d1++;
-            left = left->left;
-        }
-        while (right)
-        {
-            d2++;
-            right = right->right;
-        }
-        if (d1 == d2) return pow(2, d1 + 1) - 1;
-
-        return 1 + countNodes(root->left) + countNodes(root->right);
-    }
-};
+//        for (int i = 1; i <= n; i++)
+//        {
+//            for (int j = 0; j <= amount; j++)
+//            {
+//                // 当前位置的钱币不选
+//                dp[i][j] = dp[i - 1][j];
+//                // 当前位置选
+//                if (j >= coins[i - 1])
+//                {
+//                    dp[i][j] = min(dp[i][j], dp[i][j - coins[i - 1]] + 1);
+//                }
+//            }
+//        }
+//        return dp[n][amount] == 0x3f3f3f3f ? -1 : dp[n][amount];
+//    }
+//};
 
 
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size(), m = matrix[0].size();
-        int x = -1, y = 0;
-        for (int i = 0; i < n; i++)
+    int climbStairs(int n) {
+        int dp[n + 2]; // dp[i] 表示达到第i阶的方法
+        dp[1] = 1;
+        dp[2] = 2;
+        if (n <= 2) return n;
+        for (int i = 3; i <= n; i++)
         {
-            if (matrix[i][0] > target)
-            {
-                // 必在上一行
-                x = i - 1;
-                break;
-            }
-            else if (matrix[i][0] == target) return true;
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if (x == -1) x = n - 1;
-        cout << x << endl;
-        for (int i = 0; i < m; i++)
-        {
-            if (matrix[x][i] == target) return true;
-        }
-
-        return false;
+        return dp[n];
     }
 };
-
 int main()
 {
 	cout << (int)'6' << endl;
