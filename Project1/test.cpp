dbd95d7e27e5ecb1184afe2070a3a864040a064a
qazwsxedc123y <1476303287@qq.com>
2026-07-14 18:46:29 +0800

    练习

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -1,121 +1,116 @@
-#define  _CRT_SECURE_NO_WARNINGS
-
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//int main() {
-//    string str;
-//    int begin = 0, end = 0, ans = 0;
-//    int left = 0, right = 0;
-//    cin >> str;
-//    int n = str.size();
-//    while (right < n)
-//    {
-//        if ((str[right] >= 'a' && str[right] <= 'z') || (str[right] >= 'A' && str[right] <= 'Z')) // 不是数字
-//        {
-//            right++;
-//            left = right;
-//        }
-//        else
-//        {
-//            while (right < n && str[right] >= '0' && str[right] <= '9')
-//            {
-//                right++;
-//            }
-//            int len = right - left;
-//            if (len > ans)
-//            {
-//                ans = len;
-//                begin = left;
-//                end = right - 1;
-//            }
-//            left = right;
-//        }
-//    }
-//    cout << str.substr(begin, ans) << endl;
-//}
-//// 64 位输出请用 printf("%lld")
+#include <iostream>
+using namespace std;
+int func(int a, int b)
+{
+    int c = 0;
+    while (1)
+    {
+        if (a < b) swap(a, b);
+        c = a - b;
+        if (b == c || c == 0) return b;
+        a = b;
+        b = c;
+    }
+    return 1;
+}
+int main() {
+    int a, b;
+    cin >> a >> b;
+    // a * b * 最小公因数
+    int ans = func(a, b);
+    long long ret = a * b / ans;
+    cout << ret << endl; // 大数相乘
+}
+// 64 位输出请用 printf("%lld")
 
 
-
+#include <queue>
 class Solution {
 public:
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      *
-     * 判断岛屿数量
-     * @param grid char字符型vector<vector<>>
+     * max increasing subsequence
+     * @param arr int整型vector the array
      * @return int整型
      */
-    const int N = 205;
-    int n, m;
-    int dx[4] = { -1, 1, 0, 0 };
-    int dy[4] = { 0, 0, -1, 1 };
-    void dfs(int x, int y, vector<vector<char> >& grid, vector<vector<bool> >& vis)
-    {
-        for (int k = 0; k < 4; k++)
-        {
-            int a = x + dx[k], b = y + dy[k];
-            if (a >= 0 && a < n && b >= 0 && b < m && grid[a][b] == '1' && vis[a][b] == false)
-            {
-                vis[a][b] = true;
-                dfs(a, b, grid, vis);
-            }
-        }
-    }
-    int solve(vector<vector<char> >& grid) {
+    int MLS(vector<int>& arr) {
         // write code here
-        n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> vis(N, vector<bool>(N));
         int ret = 0;
-        for (int i = 0; i < n; i++)
+        priority_queue<int> q;
+        for (auto e : arr)
         {
-            for (int j = 0; j < m; j++)
-            {
-                if (grid[i][j] == '1' && vis[i][j] == false)
-                {
-                    ret++;
-                    vis[i][j] = true;
-                    dfs(i, j, grid, vis);
-                }
-            }
+            q.push(e);
         }
+        int ans = q.top();
+        q.pop();
+        int len = 1;
+        while (!q.empty())
+        {
+            int t = q.top();
+            q.pop();
+            // cout << ans << " " << t << " " << len << endl;
+            if (t == ans - 1)
+            {
+                len++;
+            }
+            else if (t == ans)
+            {
+                continue;
+            }
+            else
+            {
+                ret = max(ret, len);
+                len = 1;
+            }
+            ans = t;
+        }
+        ret = max(ret, len);
         return ret;
     }
 };
 
 
-
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
-
+const int N = 510;
 int main() {
-    int t;
-    cin >> t;
-    vector<int> v(6);
-    while (t--) {
-        cin >> v[0] >> v[1] >> v[2] >> v[3] >> v[4] >> v[5];
-        sort(v.begin(), v.end());
-
-        if ((v[0] + v[1] > v[2] && v[3] + v[4] > v[5]) ||
-            (v[0] + v[1] > v[3] && v[2] + v[4] > v[5]) ||
-            (v[0] + v[1] > v[4] && v[2] + v[3] > v[5]) ||
-            (v[0] + v[1] > v[5] && v[2] + v[3] > v[4]) ||
-            (v[1] + v[2] > v[3] && v[0] + v[4] > v[5]) ||
-            (v[1] + v[2] > v[4] && v[0] + v[3] > v[5]) ||
-            (v[1] + v[2] > v[5] && v[0] + v[3] > v[4]) ||
-            (v[1] + v[3] > v[4] && v[0] + v[2] > v[5]) ||
-            (v[1] + v[3] > v[5] && v[0] + v[2] > v[4]) ||
-            (v[0] + v[2] > v[3] && v[1] + v[4] > v[5])) {
-            cout << "Yes" << endl;
-        }
-        else {
-            cout << "No" << endl;
+    int n, m;
+    cin >> n >> m;
+    int dp[N][N]; // dp[i + 1][j + 1] 表示到达i,j处最多得分
+    vector<vector<char>> v(N, vector<char>(N));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> v[i][j];
         }
     }
+
+    int t;
+    if (v[0][0] == 'l') t = 4;
+    else if (v[0][0] == 'o') t = 3;
+    else if (v[0][0] == 'v') t = 2;
+    else if (v[0][0] == 'e') t = 1;
+    dp[1][1] = t;
+
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (i == 1 && j == 1) continue;
+            int t = 0;
+            if (v[i - 1][j - 1] == 'l') t = 4;
+            else if (v[i - 1][j - 1] == 'o') t = 3;
+            else if (v[i - 1][j - 1] == 'v') t = 2;
+            else if (v[i - 1][j - 1] == 'e') t = 1;
+
+            dp[i][j] = t + max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    cout << dp[n][m] << endl;
     return 0;
 }
-// 64 位输出请用 printf("%lld")
+// 64 位输出请用 printf("%lld")2 3
